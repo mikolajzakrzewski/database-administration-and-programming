a7a3fa810aa01b28c764439c5597efac50af0c83
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-11-13 21:38:57 +0100

    Corrected version of activity 3.8

--- activity_3/a3z8.sql
+++ activity_3/a3z8.sql
@@ -1,47 +1,46 @@
 -- 8. Stwórz blok anonimowy i zadeklaruj w nim odpowiednie zmienne oraz kursor. Usuń wszystkie lokalizacje, w których nie ma żadnego departamentu. W rozwiązaniu wykorzystaj klauzulę WHERE CURRENT OF w odniesieniu do kursora.
 
--- Postgres
+-- PostgreSQL
 DO $$
 DECLARE
-    curs CURSOR FOR
-        SELECT l.location_id
-        FROM locations AS l
-        LEFT JOIN departments AS d
-            ON d.location_id = l.location_id
-        WHERE d.department_id IS NULL
-        FOR UPDATE OF l;
-loc_id INT;
+	loc_id INT;
+	curs CURSOR FOR
+		SELECT l.location_id
+		FROM locations AS l
+		LEFT JOIN departments AS d
+			ON d.location_id = l.location_id
+		WHERE d.department_id IS NULL
+		FOR UPDATE OF l;
 BEGIN
-    OPEN curs;
-    LOOP
-        FETCH curs INTO loc_id;
-        EXIT WHEN NOT FOUND;
-        DELETE FROM locations WHERE CURRENT OF curs;
-    END LOOP;
-    CLOSE curs;
+	OPEN curs;
+	LOOP
+		FETCH curs INTO loc_id;
+		EXIT WHEN NOT FOUND;
+		DELETE FROM locations WHERE CURRENT OF curs;
+	END LOOP;
+	CLOSE curs;
 END $$;
 
--- MSSQL
+-- Microsoft SQL Server
+DECLARE
+	@location_id NUMERIC(4);
 DECLARE department_cursor CURSOR FOR
-    SELECT *
-    FROM locations AS l
-    LEFT JOIN departments AS d
-        ON d.location_id = l.location_id
-    WHERE d.department_id IS NULL
-
-OPEN department_cursor
-
-FETCH NEXT FROM department_cursor
-
-WHILE @@FETCH_STATUS = 0
+	SELECT l.location_id
+	FROM locations AS l
+	LEFT JOIN departments AS d
+		ON d.location_id = l.location_id
+	WHERE d.department_id IS NULL;
 BEGIN
-    DELETE FROM locations WHERE CURRENT OF department_cursor
-    FETCH NEXT FROM department_cursor
-END
-
-CLOSE department_cursor
-DEALLOCATE department_cursor;
+	OPEN department_cursor;
+	FETCH NEXT FROM department_cursor INTO @location_id;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		DELETE FROM locations WHERE CURRENT OF department_cursor;
+		FETCH NEXT FROM department_cursor INTO @location_id;
+	END;
+	CLOSE department_cursor;
+	DEALLOCATE department_cursor;
+END;
 
 -- Dodatkowo wymień wszystkie polecenia, z którymi można wykorzystać klauzulę WHERE CURRENT OF w odniesieniu do kursora.
-
 -- Odp: UPDATE, DELETE
