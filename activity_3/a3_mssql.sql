d04236c0bcc897597059c3bece7f77615c76d077
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-11-18 12:58:07 +0100

    Activities 3 and 4 templates

---
+++ activity_3/a3_mssql.sql
@@ -0,0 +1,116 @@
+-- 1. Dla każdego pracownika, wyświetl jego imię, nazwisko oraz informację o prowizji:
+--        – "Brak prowizji" w przypadku, gdy pracownik nie ma podanej prowizji,
+--        – "Nieznana prowizja" w przypadku, gdy prowizja pracownika nie może być porównana względem pozostałych
+--          pracowników z tego samego działu,
+--        – "Niska prowizja" w przypadku, gdy iloczyn prowizji i minimalnego wynagrodzenia dla stanowiska pracownika
+--          jest mniejszy niż średnie wynagrodzenie wszystkich pracowników z działu tego pracownika pomniejszone o 5000,
+--        – "Wysoka prowizja" w pozostałych przypadkach.
+--    Kolumnę z informacją o prowizji nazwij commission_info.
+--    Wynik posortuj według ostatniej informacji.
+--    W rozwiązaniu wykorzystaj instrukcję warunkową.
+
+-- Microsoft SQL Server
+
+
+
+-- 2. Wyświetl nazwy krajów, nazwy regionów oraz liczby departamentów znajdujących się w każdym kraju.
+--    Wyniki ogranicz w zależności od liczby departamentów w następujący sposób:
+--        – uwzględnij tylko te kraje z regionu Europe, które mają więcej niż 1 departament,
+--        – uwzględnij tylko te kraje z regionu Americas, które mają więcej niż 3 departamenty,
+--        – nie uwzględniaj krajów z pozostałych regionów.
+--    W rozwiązaniu wykorzystaj instrukcję warunkową CASE w klauzuli HAVING.
+--    Dodatkowo wskaż pozostałe klauzule, w których także można wykorzystać instrukcję warunkową CASE.
+
+-- Microsoft SQL Server
+
+
+
+-- 3. Przeanalizuj poniższy ciąg wartości i znajdź zależności:
+--      5.
+--      4.
+--      4.1.
+--      4.3.
+--      4.4.
+--      3.
+--      3.1.
+--      3.3.
+--      2.
+--      2.1.
+--      1.
+--      1.1.
+--    Napisz blok anonimowy, który wypisze powyższe wartości.
+--    W rozwiązaniu wykorzystaj pętlę LOOP oraz funkcje wyjścia, kontynuowania i/lub przerwania iteracji.
+--    Uwaga! Jeżeli w jakimś systemie nie istnieje pętla LOOP, wykorzystaj inny, dostępny rodzaj pętli.
+
+-- Microsoft SQL Server
+
+
+
+-- 4. Stwórz blok anonimowy i zadeklaruj w nim odpowiednie zmienne.
+--    Wypisz nazwy kolejnych miast zaczynając od lokalizacji z ID równym 1500 i kończąc na lokalizacji z ID równym 2500.
+--    Załóż, że w bazie danych nie brakuje żadnych wartości ID lokalizacji w powyższym przedziale, gdzie krok
+--    wynosi 100.
+--    Dla każdego miasta wypisz dodatkowo tyle par nawiasów prostokątnych ( [] ), ile departamentów znajduje się w nim.
+--    Uwaga! Jeżeli dany system to umożliwia, wykorzystaj %TYPE przy deklaracji zmiennych.
+--    Uwaga! Jeżeli dany system to umożliwia, użyj 1 pętli FOR oraz 1 pętli WHILE.
+
+-- Microsoft SQL Server
+
+
+
+-- 5. Stwórz blok anonimowy i zadeklaruj w nim zmienne dla sumy wynagrodzeń i kwoty granicznej.
+--    Jeżeli suma wynagrodzeń jest mniejsza lub równa określonej kwocie granicznej, wypisz sumę wynagrodzeń
+--    wszystkich pracowników. W przeciwnym razie wywołaj tylko swój wyjątek i wypisz adekwatną informację.
+--    Wypróbuj swoje rozwiązanie dla kwot granicznych 500 000 i 700 000.
+--    Dodatkowo wskaż różnice pomiędzy:
+--        – THROW i RAISERROR w Microsoft SQL Server,
+--        – RAISE EXCEPTION, RAISE WARNING, RAISE NOTICE i RAISE INFO w PostgreSQL.
+
+-- Microsoft SQL Server
+
+
+
+-- 6. Stwórz blok anonimowy i zadeklaruj w nim zmienne dla nazwy departamentu i nazwy miasta.
+--    Wypisz nazwę departamentu, który znajduje się w wybranym mieście.
+--    Złap wyjątki systemowe za pomocą ich nazw w przypadkach, gdy taki departament nie istnieje lub istnieje więcej
+--    niż 1 taki departament - wypisz adekwatną informację.
+--    Wypróbuj swoje rozwiązanie dla miast Venice, Munich i Seattle.
+--    Dodatkowo sprawdź, w jaki sposób zapisywane są wartości do zmiennych przy poleceniu SELECT, które zwraca więcej
+--    niż jedną wartość lub nie zwraca nic.
+--    Uwaga! Jeżeli w jakimś systemie nie istnieją wyjątki systemowe dotyczące braku wyników lub zbyt dużej liczby
+--    wyników, zaproponuj jak najprostsze rozwiązanie, które umożliwi przechwycenie takich błędów.
+
+-- Microsoft SQL Server
+
+
+
+-- 7. Stwórz blok anonimowy i zadeklaruj w nim odpowiednie zmienne oraz kursor z parametrem odnoszącym się do nazwy
+--    kraju.
+--    Wypisz numery ID lokalizacji oraz nazwy miast z kraju United States of America, którego nazwa jest przesyłana
+--    do kursora.
+--    Uwaga! Jeżeli w jakimś systemie nie istnieje kursor z parametrem, zapisz nazwę podanego kraju w zmiennej
+--    i wykorzystaj ją w kursorze.
+
+-- Microsoft SQL Server
+
+
+
+-- 8. Stwórz blok anonimowy i zadeklaruj w nim odpowiednie zmienne oraz kursor.
+--    Usuń wszystkie lokalizacje, w których nie ma żadnego departamentu.
+--    W rozwiązaniu wykorzystaj klauzulę WHERE CURRENT OF w odniesieniu do kursora.
+--    Dodatkowo wymień wszystkie polecenia, z którymi można wykorzystać klauzulę WHERE CURRENT OF w odniesieniu
+--    do kursora.
+
+-- Microsoft SQL Server
+
+
+
+-- 9. Stwórz blok anonimowy i zadeklaruj w nim odpowiednie zmienne.
+--    Dla każdej lokalizacji, wypisz wszystkie dane na jej temat razem z dodanymi opisami.
+--    Kolejność danych w jednej wiadomości to: ID lokalizacji, adres, kod pocztowy, miasto, stan/prowincja oraz
+--    id kraju. Zwróć uwagę, żeby nie wyświetlać poszczególnych opisów w przypadku braku podanej wartości.
+--    Uwaga! Jeżeli dany system to umożliwia, wykorzystaj kursor z FOR LOOP.
+--    Uwaga! Jeżeli dany system to umożliwia, użyj %ROWTYPE przy deklaracji zmiennej.
+
+-- Microsoft SQL Server
+
---
+++ activity_4/a4_postgres.sql
@@ -0,0 +1,87 @@
+-- 1. Utwórz procedurę proc1, która z wykorzystaniem kursora wypisze nazwy miast, w których realne maksymalne zarobki
+--    pracowników są niższe od podanej kwoty.
+--    Wywołaj ją z parametrem 10000.
+
+-- PostgreSQL
+
+
+
+-- 2. Utwórz procedurę proc2, która dodaje informacje o nowym departamencie do bazy danych.
+--    ID nowego departamentu musi być automatycznie wyliczane zgodnie z zasadą nadawania ID dla departamentów.
+--    Nazwa departamentu musi być podana jako parametr procedury.
+--    ID menadżera domyślnie nie ma wpisywanej wartości, ale można ją podać jako parametr procedury.
+--    ID lokalizacji ma domyślnie ustawioną wartość 2000, jednakże można podać także inną wartość jako parametr
+--    procedury.
+--    Wywołaj procedurę proc2 na wszystkie możliwe sposoby, żeby przetestować działanie parametrów domyślnych.
+
+-- PostgreSQL
+
+
+
+-- 3. Utwórz procedurę proc3, która podwyższy prowizję o podaną liczbę punktów procentowych u pracowników zatrudnionych
+--    przed podanym rokiem i poprzez parametr wyjściowy zwróci liczbę zmodyfikowanych rekordów.
+--    Wywołaj ją z parametrami 2004 oraz 0.05.
+
+-- PostgreSQL
+
+
+
+-- 4. Utwórz funkcję func4, która zwróci procentowy udział liczby pracowników zatrudnionych w podanym departamencie
+--    w łącznej liczbie wszystkich pracowników.
+--    Wynik zaokrąglij do części setnych.
+--    Wywołaj ją dla wszystkich departamentów wewnątrz zapytania dającego wynik w postaci trzech kolumn: department_id,
+--    department_name, percentage.
+
+-- PostgreSQL
+
+
+
+-- 5. Utwórz funkcję func5, która zwróci wszystkie informacje o departamentach mających siedzibę w podanym kraju.
+--    Wywołaj ją z parametrem Canada wewnątrz zapytania dającego wynik w postaci dwóch kolumn: department_id,
+--    department_name.
+
+-- PostgreSQL
+
+
+
+-- 6. Utwórz funkcję func6, która zwróci kursor z informacjami o pracownikach (imię, nazwisko oraz nazwa stanowiska),
+--    których menadżerem jest podany pracownik.
+--    Wywołaj ją z parametrami "Matthew" i "Weiss".
+--    Następnie wypisz spośród nich tylko tych pracowników (ich imiona i nazwiska), którzy zajmują stanowisko
+--    Stock Clerk.
+--    Uwaga! Jeżeli w jakimś systemie nie istnieje funkcja zwracająca kursor, wykorzystaj funkcję zwracającą tabelę
+--    oraz kursor.
+
+-- PostgreSQL
+
+
+
+-- 7. Utwórz wyzwalacz, który przed dodaniem pracownika sprawdzi, czy data zatrudnienia jest przyszłą datą.
+--    Jeżeli warunek jest spełniony, to wyświetli tylko komunikat "Niedozwolona operacja!".
+--    Jeżeli warunek nie jest spełniony, to doda pracownika.
+--    Potwierdź działanie dla dwóch przypadków testowych.
+--    Uwaga! W niektórych systemach wymagane jest utworzenie własnej funkcji i/lub widoku tabeli pracownicy.
+
+-- PostgreSQL
+
+
+
+-- 8. Utwórz wyzwalacz, który po usunięciu jednego lub wielu miast za pomocą pojedynczego polecenia wypisze ich nazwy
+--    oraz nazwy ich krajów.
+--    Potwierdź działanie poprzez usunięcie wszystkich miast, w których żaden departament nie ma swojej siedziby.
+--    Uwaga! W niektórych systemach wymagane jest utworzenie kursora lub własnej funkcji.
+
+-- PostgreSQL
+
+
+
+-- 9. Utwórz wyzwalacz, który przed podwyższeniem prowizji menadżera departamentu sprawdzi jej nową wartość.
+--    Jeżeli nowa prowizja jest co najmniej dwukrotnie większa od poprzedniej, to zmniejszy jej nową wartość
+--    do dwukrotności poprzedniej wartości. Jeżeli menadżer departamentu nie miał wcześniej przypisanej prowizji,
+--    nowa wartość może wynieść maksymalnie 0,1. Potwierdź działanie wyzwalacza aktualizując wybranych pracowników
+--    z departamentów o id równym 20 i 80.
+--    Uwaga! W niektórych systemach wymagane jest utworzenie własnej funkcji.
+--    Uwaga! Jeżeli w jakimś systemie nie istnieje wyzwalacz BEFORE, wykorzystaj wyzwalacz INSTEAD OF.
+
+-- PostgreSQL
+
